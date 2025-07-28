e4115351b980abf854c6e8a1f6ecd60a52abbd78
jiangnan <764234162@qq.com>
2025-07-28 14:46:37 +0800

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
+import createMDX from '@next/mdx';
+import remarkGfm from 'remark-gfm';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  experimental: {
+    mdxRs: true,
+  },
+  output: 'export',
+  basePath: '/JY764234162',
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true,
+  distDir: 'docs',
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+});
+
+export default withMDX(nextConfig);
